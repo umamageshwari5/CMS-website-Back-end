69e72e40c265e80d2342e2659c3462e31fa8fd87
Uma-Mageshwari-S <umamagesh1301@gmail.com>
2025-09-24 13:54:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -92,11 +92,12 @@ app.delete(
   }
 );
 
-// app.get("/", (req, res) => {
-//   res.send("Server is running! This is the backend.");
-// });
+app.get("/", (req, res) => {
+  res.send("Server is running! This is the backend.");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
